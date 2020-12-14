5616b513bd12f0d8cb9715fe8b05a0715e43d513
Abdelhay <abdelhaygamaleldin@gmail.com>
2020-12-14 07:42:05 +0100

    new RDS,EC2 and Bucket

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // baseUrl:"http://ec2-54-212-95-32.us-west-2.compute.amazonaws.com:9191"
-  baseUrl:"http://ec2-18-237-66-248.us-west-2.compute.amazonaws.com"
+  baseUrl:"http://ec2-18-237-230-69.us-west-2.compute.amazonaws.com"
 };
