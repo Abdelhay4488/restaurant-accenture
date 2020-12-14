14d1e76a8a8c16b644949ade136b41eb9a291f3c
Abdelhay <abdelhaygamaleldin@gmail.com>
2020-12-14 09:41:54 +0100

    new cloud Template

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // baseUrl:"http://ec2-54-212-95-32.us-west-2.compute.amazonaws.com:9191"
-  baseUrl:"http://ec2-18-237-230-69.us-west-2.compute.amazonaws.com"
+  baseUrl:"http://ec2-34-222-237-95.us-west-2.compute.amazonaws.com"
 };
+
